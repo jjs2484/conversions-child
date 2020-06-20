b670b4dbd04dc0ce7b55652f567ccb811c206919
uniquelylost <jsneider24@gmail.com>
2020-06-20 05:26:12 -0400

    start updating shortcode paths

--- functions.php
+++ functions.php
@@ -33,7 +33,7 @@ Creates the following element shortcodes:
 add_shortcode('conversions_clients', 'conversions_clients_shortcode');
 if ( ! function_exists( 'conversions_clients_shortcode' ) ) {
 	function conversions_clients_shortcode() {
-		$homepage = new conversions\homepage();
+		$homepage = new conversions\extensions\homepage\Homepage();
 		return '<section class="c-clients c-shortcode mt-4 mb-4"><div class="row">'.$homepage->clients_content().'</div></section>';
 	}
 }
@@ -42,7 +42,7 @@ if ( ! function_exists( 'conversions_clients_shortcode' ) ) {
 add_shortcode('conversions_features', 'conversions_features_shortcode');
 if ( ! function_exists( 'conversions_features_shortcode' ) ) {
 	function conversions_features_shortcode() {
-		$homepage = new conversions\homepage();
+		$homepage = new conversions\extensions\homepage\Homepage();
 		return '<section class="c-features c-shortcode mt-4 mb-2"><div class="row">'.$homepage->features_content().'</div></section>';
 	}
 }
@@ -51,7 +51,7 @@ if ( ! function_exists( 'conversions_features_shortcode' ) ) {
 add_shortcode('conversions_pricing', 'conversions_pricing_shortcode');
 if ( ! function_exists( 'conversions_pricing_shortcode' ) ) {
 	function conversions_pricing_shortcode() {
-		$homepage = new conversions\homepage();
+		$homepage = new conversions\extensions\homepage\Homepage();
 		return '<section class="c-pricing c-shortcode mt-4 mb-2"><div class="row">'.$homepage->pricing_content().'</div></section>';
 	}
 }
@@ -60,7 +60,7 @@ if ( ! function_exists( 'conversions_pricing_shortcode' ) ) {
 add_shortcode('conversions_testimonials', 'conversions_testimonials_shortcode');
 if ( ! function_exists( 'conversions_testimonials_shortcode' ) ) {
 	function conversions_testimonials_shortcode() {
-		$homepage = new conversions\homepage();
+		$homepage = new conversions\extensions\homepage\Homepage();
 		return '<section class="c-testimonials c-shortcode mt-4 mb-2"><div class="row">'.$homepage->testimonials_content().'</div></section>';
 	}
 }

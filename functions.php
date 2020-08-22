adc2cf7facba462481c807a97005c07d009d571e
Jesse S <3428918+jjs2484@users.noreply.github.com>
2020-08-22 09:15:40 -0400

    remove shortcodes

--- functions.php
+++ functions.php
@@ -20,49 +20,4 @@ function conversions_child_enqueue_styles() {
     );
 }
 
-/*
-Creates the following element shortcodes:
-
-[conversions_clients]
-[conversions_features]
-[conversions_pricing]
-[conversions_testimonials]
-*/
-
-// Create clients carousel shortcode
-add_shortcode('conversions_clients', 'conversions_clients_shortcode');
-if ( ! function_exists( 'conversions_clients_shortcode' ) ) {
-	function conversions_clients_shortcode() {
-		$homepage = new conversions\extensions\homepage\Homepage();
-		return '<section class="c-clients c-shortcode mt-4 mb-4"><div class="row">' . $homepage->clients_content() . '</div></section>';
-	}
-}
-
-// Create features shortcode
-add_shortcode('conversions_features', 'conversions_features_shortcode');
-if ( ! function_exists( 'conversions_features_shortcode' ) ) {
-	function conversions_features_shortcode() {
-		$homepage = new conversions\extensions\homepage\Homepage();
-		return '<section class="c-features c-shortcode mt-4 mb-2"><div class="row">' . $homepage->features_content() . '</div></section>';
-	}
-}
-
-// Create pricing tables shortcode
-add_shortcode('conversions_pricing', 'conversions_pricing_shortcode');
-if ( ! function_exists( 'conversions_pricing_shortcode' ) ) {
-	function conversions_pricing_shortcode() {
-		$homepage = new conversions\extensions\homepage\Homepage();
-		return '<section class="c-pricing c-shortcode mt-4 mb-2"><div class="row">' . $homepage->pricing_content() . '</div></section>';
-	}
-}
-
-// Create testimonials shortcode
-add_shortcode('conversions_testimonials', 'conversions_testimonials_shortcode');
-if ( ! function_exists( 'conversions_testimonials_shortcode' ) ) {
-	function conversions_testimonials_shortcode() {
-		$homepage = new conversions\extensions\homepage\Homepage();
-		return '<section class="c-testimonials c-shortcode mt-4 mb-2"><div class="row">' . $homepage->testimonials_content() . '</div></section>';
-	}
-}
-
 /* Your custom functions go here */
